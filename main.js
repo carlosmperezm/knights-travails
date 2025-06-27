2e2db85054f97f14a0650d437173a1a3ebeaed4f
CarlosPerez <carlosmperez2901@gmail.com>
2025-06-27 15:50:05 -0700

    Solution

--- main.js
+++ main.js
@@ -13,10 +13,6 @@
  *  previous position before it, and so on until reach the satarted position.
  *
  */
-
-let path = [];
-let firstPositions;
-
 class Position {
   x;
   y;
@@ -29,12 +25,9 @@ class Position {
     this.y = coordinates[1];
     this.coordinates = coordinates;
   }
-
   get children() {
     return this.#getSurroundedValidPositions();
   }
-
-
   #getSurroundedValidPositions() {
     const surroundedPositions = [
       new Position([this.x - 2, this.y - 1]),
@@ -57,53 +50,36 @@ class Position {
 
 }
 
-
-
 function knightMoves(startPosition, endPosition) {
-
   const firstPosition = new Position(startPosition);
   const finalPosition = new Position(endPosition);
-
   const positionsToVisit = [firstPosition];
   const visitedPositions = [];
 
-  let counter = 0;
-
   while (positionsToVisit.length != 0) {
-    if (counter === 60) return; // Just to make sure the algorithm doesn't go to and infine loop
-    counter++;
-
     // Take the next position in the queue
     const position = positionsToVisit.shift();
-    console.log('Extracting from the queue Position: ', position.coordinates)
-    console.log('Whose parent is: ', position.parentPosition)
     // Add the position to the visited list
     visitedPositions.push(position);
 
-
     // If position is found
     if (position.x === finalPosition.x && position.y === finalPosition.y) {
-      // console.log('Postions in QUEUE: ', positionsToVisit)
-      console.log('Postions in visited: ', visitedPositions)
-      console.log('Position FOUND:')
       // Return the path
       path = [];
       let pos = position
       // Add all the positions that has been used to get to the final Position
       while (pos) {
-        console.log('Taking the parent of: ', pos)
         path.unshift(pos.coordinates)
-        console.log("Buidling path: ", path)
         pos = pos.parentPosition;
       }
-      console.log('PATH: ', path)
-      return path;
+      console.log(`You made it in ${path.length - 1} moves!  Here's your path:`)
+      path.forEach(p => console.log(p))
+      return;
     }
 
     // If Position is NOT found
     const nextPositions = position.children;
 
-    console.log('------------------------Children-----------------------')
     for (let pos of nextPositions) {
       // Validate if the position is in the queue
       let isChildInQueue = positionsToVisit.find(element => {
@@ -117,22 +93,16 @@ function knightMoves(startPosition, endPosition) {
       // Is a valid position
       if (!isChildInQueue && !wasChildVisited) {
         const childPosition = new Position(pos.coordinates);
-        console.log('Position: ', childPosition.coordinates, 'Adding Parent: ', position.coordinates)
         childPosition.parentPosition = position;
-        console.log('Adding to the queue: ', childPosition)
         positionsToVisit.push(childPosition);
-        console.log('Checking on the Postions to visit list: ', positionsToVisit)
-      } else {
-        console.log('Skipping: ', pos.coordinates)
       }
     }
-    console.log('------------------------Children ENDS-----------------------')
   }
 
 }
 
-// knightMoves([3, 3], [0, 0]);
-// knightMoves([0, 0], [3, 3]);
-// knightMoves([0, 0], [7, 7]); 
+knightMoves([0, 0], [3, 3]);
+knightMoves([3, 3], [0, 0]);
+knightMoves([0, 0], [7, 7]);
 knightMoves([3, 3], [4, 3])
 
