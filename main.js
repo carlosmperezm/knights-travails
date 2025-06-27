fab9c4535b2860b011b5d5ed1f26cee15de0b8d8
CarlosPerez <carlosmperez2901@gmail.com>
2025-06-27 15:15:27 -0700

    Catch the last position and build a path based on poisitions parents

--- main.js
+++ main.js
@@ -20,151 +20,114 @@ let firstPositions;
 class Position {
   x;
   y;
-  parentPosition = [];
-  children = [];
+  parentPosition;
+  #children = [];
+  coordinates = [];
 
   constructor(coordinates) {
     this.x = coordinates[0];
     this.y = coordinates[1];
-    this.children = getSurroundedValidPositions();
+    this.coordinates = coordinates;
   }
 
-  getSurroundedValidPositions() {
+  get children() {
+    return this.#getSurroundedValidPositions();
+  }
+
+
+  #getSurroundedValidPositions() {
     const surroundedPositions = [
-      [this.x - 2, this.y - 1],
-      [this.x - 1, this.y - 2],
-      [this.x + 1, this.y - 2],
-      [this.x + 2, this.y - 1],
-      [this.x + 2, this.y + 1],
-      [this.x + 1, this.y + 2],
-      [this.x - 1, this.y + 2],
-      [this.x - 2, this.y + 1],
+      new Position([this.x - 2, this.y - 1]),
+      new Position([this.x - 1, this.y - 2]),
+      new Position([this.x + 1, this.y - 2]),
+      new Position([this.x + 2, this.y - 1]),
+      new Position([this.x + 2, this.y + 1]),
+      new Position([this.x + 1, this.y + 2]),
+      new Position([this.x - 1, this.y + 2]),
+      new Position([this.x - 2, this.y + 1]),
     ];
     const validPositions = surroundedPositions.filter(position => {
-      return position[0] >= 0
-        && position[0] <= 7
-        && position[1] >= 0
-        && position[1] <= 7;
+      return position.x >= 0
+        && position.x <= 7
+        && position.y >= 0
+        && position.y <= 7;
     });
     return validPositions;
   }
 
 }
 
-function getPath(startPosition, endPosition) {
-  let positions = getSurroundedValidPositions(startPosition);
-  if (path.length == 0) {
-    firstPositions = positions;
-  }
-  path.push(startPosition);
-  console.log('poisitions of :', startPosition);
-  console.log(positions)
-
-  for (let position of positions) {
-    if (position[0] === endPosition[0] && position[1] === endPosition[1]) {
-      console.log('Position Found: ', position)
-      path.push(position);
-      return path;
-    }
-  }
-  let position = positions.pop();
-  let isPositionInPathList = path.find(pos => {
-    return position[0] === pos[0] && position[1] === pos[1];
-  })
-  let isPositionInFirstPositions = firstPositions.find(pos => {
-    return position[0] === pos[0] && position[1] === pos[1];
-  })
-  if (isPositionInPathList || isPositionInFirstPositions) {
-    console.log('Skipping: ', position);
-    console.log('Positions: ', positions)
-    position = positions.pop();
-    // return;
-  }
-  if (path.length > 20) return;
-  console.log('Path: ', path)
-  path.concat(getPath(position, endPosition));
-  return path;
-}
-
-function getSurroundedValidPositions(position) {
-  let startX = position[0];
-  let startY = position[1];
-  const surroundedPositions = [
-    [startX - 2, startY - 1],
-    [startX - 1, startY - 2],
-    [startX + 1, startY - 2],
-    [startX + 2, startY - 1],
-    [startX + 2, startY + 1],
-    [startX + 1, startY + 2],
-    [startX - 1, startY + 2],
-    [startX - 2, startY + 1],
-  ];
-  const validPositions = surroundedPositions.filter(position => {
-    return position[0] >= 0 && position[0] <= 7 && position[1] >= 0 && position[1] <= 7;
-  })
-  return validPositions;
-}
 
 // console.log(getPath([0, 0], [3, 3]))
 // console.log(getPath([3, 3], [0, 0]))
 
 function knightMoves(startPosition, endPosition) {
-  const queue = [startPosition];
-  const visitedPositions = [];
-  const path = {
 
-  }
+  const firstPosition = new Position(startPosition);
+  const finalPosition = new Position(endPosition);
+
+  const positionsToVisit = [firstPosition];
+  const visitedPositions = [];
+
   let counter = 0;
 
-  while (queue.length > 0) {
-    if (counter === 60) return;
+  while (positionsToVisit.length != 0) {
+    if (counter === 60) return; // Just to make sure the algorithm doesn't go to and infine loop
     counter++;
-    let position = queue.shift();
+
+    // Take the next position in the queue
+    const position = new Position(positionsToVisit.shift().coordinates);
+    console.log('Extracting from the queue Position: ', position.coordinates)
+    console.log('Whose parent is: ', position.parentPosition)
+    // Add the position to the visited list
     visitedPositions.push(position);
-    console.log('Position: ', position)
-    console.log('Queue: ', queue);
-    console.log('Visited Positions', visitedPositions);
-    console.log('Path: ', path);
 
 
     // If position is found
-    if (position[0] === endPosition[0] && position[1] === endPosition[1]) {
-      console.log('Found at: ', position);
-      console.log('Final Path: ', path)
+    if (position.x === finalPosition.x && position.y === finalPosition.y) {
+      // console.log('Postions in QUEUE: ', positionsToVisit)
+      console.log('Postions in visited: ', visitedPositions)
+      console.log('Position FOUND:')
+      // Return the path
+      path = [];
+      let pos = position
+      // Add all the positions that has been used to get to the final Position
+      while (pos) {
+        console.log('Taking the parent of: ', pos)
+        path.unshift(position.coordinates)
+        console.log("Buidling path: ", path)
+        pos = position.parentPosition;
+      }
+      console.log('PATH: ', path)
       return path;
     }
 
-    let key = position.toString();
-    path[key] = [];
+    // If Position is NOT found
+    const nextPositions = position.children;
 
-    if (path[startPosition.toString()].at(-1)) {
-      console.log('jujuejeu', path[startPosition.toString()].at(-1))
-
-      if (position.toString() === path[startPosition.toString()].at(-1).toString()) {
-        console.info("Last child of : ", startPosition);
-      }
-    }
-
-    let children = getSurroundedValidPositions(position)
-    console.log('Element: ', position, 'Positions:', children);
-
-    for (child of children) {
-      let isChildInQueue = queue.find(element => {
-        return element[0] === child[0] && element[1] === child[1];
+    console.log('------------------------Children-----------------------')
+    for (let pos of nextPositions) {
+      // Validate if the position is in the queue
+      let isChildInQueue = positionsToVisit.find(element => {
+        return element.x === pos.x && element.y === pos.y;
       });
+      // Validate if the position was visited
       let wasChildVisited = visitedPositions.find(element => {
-        return element[0] === child[0] && element[1] === child[1];
+        return element.x === pos.x && element.y === pos.y;
       });
+
+      // Is a valid position
       if (!isChildInQueue && !wasChildVisited) {
-        path[key].push(child);
-        console.log('Element: ', child, 'is NOT in the queue.')
-        console.log('Saving it...')
-        queue.push(child);
+        const childPosition = new Position(pos.coordinates);
+        console.log('Position: ', childPosition.coordinates, 'Adding Parent: ', position.coordinates)
+        childPosition.parentPosition = position;
+        console.log('Adding to the queue: ', childPosition)
+        positionsToVisit.push(childPosition);
       } else {
-        console.log('Skipping ', child)
+        console.log('Skipping: ', pos.coordinates)
       }
     }
-    // console.log('Positions: ', leftChild, rightChild)
+    console.log('------------------------Children ENDS-----------------------')
   }
 
 }
